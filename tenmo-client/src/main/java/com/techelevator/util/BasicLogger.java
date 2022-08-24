02500b9cdc1577beedbe0302bef8ef6a2eac2eda
Student A <studenta@email.com>
2022-08-24 16:33:21 -0400

    Fix issue where BasicLogger couldn't find logs folder when module-2-capstone instead of module-2-capstone/tenmo-client subfolder was loaded into IntelliJ as the root folder.

--- tenmo-client/src/main/java/com/techelevator/util/BasicLogger.java
+++ tenmo-client/src/main/java/com/techelevator/util/BasicLogger.java
@@ -1,5 +1,6 @@
 package com.techelevator.util;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
@@ -9,13 +10,24 @@ import java.time.format.DateTimeFormatter;
 
 public class BasicLogger {
 
+	private static String LOGS_SUBFOLDER = "logs";
+	private static String APPLICATION_PROJECT_SUBFOLDER = "tenmo-client";
 	private static PrintWriter pw = null;
 	
 	public static void log(String message) {
 		try {
 			if (pw == null) {
-				String logFilename = "logs/" + LocalDate.now().format(DateTimeFormatter.ISO_DATE) + ".log";
-				pw = new PrintWriter(new FileOutputStream(logFilename, true));
+				// Kludge workaround for issue where logger can't find the logs subfolder when application project is in
+				// a subfolder of the root folder loaded into IntelliJ: Check to see if logs folder can be found in root
+				// IntelliJ folder... if not than assume it is in application project subfolder.
+				File folder = new File(LOGS_SUBFOLDER);
+				String logFolderPath = folder.exists() ? LOGS_SUBFOLDER : APPLICATION_PROJECT_SUBFOLDER
+						+ File.separator + LOGS_SUBFOLDER;
+
+				String logFilePath = logFolderPath + File.separator
+						+ LocalDate.now().format(DateTimeFormatter.ISO_DATE) + ".log";
+
+				pw = new PrintWriter(new FileOutputStream(logFilePath, true));
 			}
 			pw.println(LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME) + " " + message);
 			pw.flush();
