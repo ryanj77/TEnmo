3cd5bafdd7314da30b997029f3ff2395e9158b61
Student A <studenta@email.com>
2022-08-24 13:31:28 -0400

    Implement server side portion of use case 3

--- tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/dao/JdbcAccountDao.java
@@ -1,8 +1,39 @@
 package com.techelevator.tenmo.dao;
 
+import com.techelevator.tenmo.exception.AccountNotFoundException;
+import com.techelevator.tenmo.model.Account;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Component;
 
+// actual methods to be called for each database interaction with the "account" table
 @Component
 public class JdbcAccountDao implements AccountDao{
-    //actual methods to be called for each database interaction with the "account" table
+
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcAccountDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public Account findByUsername(String username) throws Exception {
+        String sql = "SELECT account_id, account.user_id as user_id, balance " +
+                "FROM account " +
+                "JOIN tenmo_user on tenmo_user.user_id = account.user_id " +
+                "WHERE tenmo_user.username=?;";
+        SqlRowSet rowSet = jdbcTemplate.queryForRowSet(sql, username);
+        if (!rowSet.next()) {
+            throw new AccountNotFoundException(username);
+        }
+        return mapRowToAccount(rowSet);
+    }
+
+    private Account mapRowToAccount(SqlRowSet rowSet) {
+        Account account = new Account();
+        account.setAccountId(rowSet.getInt("account_id"));
+        account.setUserId(rowSet.getInt("user_id"));
+        account.setBalance(rowSet.getBigDecimal("balance"));
+        return account;
+    }
 }
--- tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/model/Account.java
@@ -3,6 +3,26 @@ package com.techelevator.tenmo.model;
 import java.math.BigDecimal;
 
 public class Account {
+    private int accountId;
+    private int userId;
+    private BigDecimal balance;
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
     public BigDecimal getBalance() {
         return balance;
     }
@@ -10,6 +30,4 @@ public class Account {
     public void setBalance(BigDecimal balance) {
         this.balance = balance;
     }
-
-    private BigDecimal balance;
 }
